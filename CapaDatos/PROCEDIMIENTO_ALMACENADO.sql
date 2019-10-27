104668af4fa8164619175b952434eef11cc4aa24
Robert Gonzalez <robert_gon97@hotmail.com>
2019-10-27 12:07:25 -0400

    Add functions to procedures.users

--- CapaDatos/PROCEDIMIENTO_ALMACENADO.sql
+++ CapaDatos/PROCEDIMIENTO_ALMACENADO.sql
@@ -9,9 +9,9 @@ GO
 
 -- Get name users
 CREATE PROC DB_USERS_FIND_NAME 
-@varName VARCHAR(50) 
+@varName VARCHAR(256) 
 AS
-	SELECT * FROM users WHERE user_nombre LIKE '%' + @varName + '%'
+	SELECT * FROM users WHERE user_name LIKE '%' + @varName + '%' OR user_username LIKE '%' + @varName + '%' OR user_id = @varName 
 GO
 
 -- Insert user
@@ -22,4 +22,21 @@ CREATE PROC DB_USERS_REGISTER
 @varPassword VARCHAR(256)
 AS
 	INSERT INTO users (user_id, user_username, user_name, user_password) VALUES (NULL, @varUser, @varName, @varPassword)
+GO
+
+-- Update user
+CREATE PROC DB_USERS_UPDATE 
+@varID INT,
+@varUser VARCHAR(50),
+@varName VARCHAR(50),
+@varPassword VARCHAR(256)
+AS
+	UPDATE users SET user_username = @varUser, user_name = @varName, user_password = @varPassword WHERE user_id = @varID
+GO
+
+-- Delete user
+CREATE PROC DB_USERS_DELETE 
+@varID INT
+AS
+	DELETE users WHERE user_id = @varID
 GO
