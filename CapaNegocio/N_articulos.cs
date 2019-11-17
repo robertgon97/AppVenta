cc7a0cd90c89f293c6ca8c2ec23acf6e7e832a2d
Robert Gonzalez <robert_gon97@hotmail.com>
2019-11-17 09:29:53 -0400

    Capar Negocio (N-articulos)

---
+++ CapaNegocio/N_articulos.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+using CapaDatos;
+using System.Data;
+
+namespace CapaNegocio
+{
+    class N_articulos
+    {
+        public static DataTable obtenerTodosLosArticulos(int miArticuloID)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miArticuloID, _fechaActual, "Obtuvo Todos Los Articulos", "El Usuario solicito todos los Articulos", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DataTable TodosLosArticulos = new DB_articulos().GetAll();
+            return TodosLosArticulos;
+        }
+
+        public static DataTable obtenerUnArticulo(int miArticuloID, int id)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miArticuloID, _fechaActual, "Obtuvo informacion un artículo", "El Usuario solicito todos los datos de un artículo", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_articulos Articulo = new DB_articulos(id, 0, 0, "0", "0", "0", "0", 0, "0");
+            DataTable ArticuloEnTabla = Articulo.GetArticleId(Articulo);
+            return ArticuloEnTabla;
+        }
+
+        public static DataTable buscarArticulos(int miArticuloID, string search)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miArticuloID, _fechaActual, "Solicito buscar informacion de un artículo", "El Usuario solicito la busqueda de todos los datos de los artículos en la base de datos", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_articulos ARticulos = new DB_articulos( 0, 0, 0, "0", "0", "0", "0", 0, search);
+            DataTable ArticuloEnTabla = ARticulos.GetSearch(ARticulos);
+            return ArticuloEnTabla;
+        }
+
+        public static string agregarArticulo(int miArticuloID, int id, int categoriaid, int presentacionid, string nombre, string codigobarra, string descripcion, string imagen, decimal precio, string buscar)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miArticuloID, _fechaActual, "Registro un nuevo artículo", "El Usuario solicito el registro de otro artículo", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_articulos nuevoArticulo = new DB_articulos(id, categoriaid, presentacionid, nombre, codigobarra, descripcion, imagen, precio, buscar);
+            return nuevoArticulo.Create(nuevoArticulo);
+        }
+
+        public static string modificarArticulo(int miArticuloID, int id, int categoriaid, int presentacionid, string nombre, string codigobarra, string descripcion, string imagen, decimal precio, string buscar)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miArticuloID, _fechaActual, "Modifico informacion un Artículo", "El Usuario solicito modificar los datos de un artículo", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_articulos actualArticulo = new DB_articulos(id, categoriaid, presentacionid, nombre, codigobarra, descripcion, imagen, precio, buscar);
+            return actualArticulo.Edit(actualArticulo);
+        }
+
+        public static string eliminarArticulo(int miArticuloID, int id)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miArticuloID, _fechaActual, "Elimino informacion un Usuario", "El Usuario solicito eliminar los datos de un artículo", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_articulos eliminarArticulo = new DB_articulos(id, 0, 0, "0", "0", "0", "0",0, "0" );
+            return eliminarArticulo.Delete(eliminarArticulo);
+        }
+    }
+
+}
+
