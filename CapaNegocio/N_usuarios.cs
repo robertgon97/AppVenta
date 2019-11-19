85a40996396ab12993e8cb822454532bd53abe2a
Robert Gonzalez <robert_gon97@hotmail.com>
2019-11-18 22:10:20 -0400

    Fix preguntas de seguridad

--- CapaNegocio/N_usuarios.cs
+++ CapaNegocio/N_usuarios.cs
@@ -74,6 +74,32 @@ namespace CapaNegocio
             return TipoEnTabla;
         }
 
+        public static DataTable obtenerPreguntaSeguridadUsuarioID(int miUsuarioID, int usuarioObjetivo)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miUsuarioID, _fechaActual, "Solicito informacion acerca de las preguntas de seguridad del usuario", "El Usuario solicito la busqueda de los datos de seguridad del usuario en la base de datos", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_preguntas preguntas = new DB_preguntas(0, usuarioObjetivo, "0", "0", "0");
+            DataTable listaDePreguntas = preguntas.GetAllPreguntaByUser(preguntas);
+            return listaDePreguntas;
+        }
+
+        public static DataTable ObtenerPreguntID(int miUsuarioID, int usuarioObjetivo)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miUsuarioID, _fechaActual, "Solicito informacion una pregunta de seguridad del usuario", "El Usuario solicito la busqueda de los datos de seguridad de un usuario en la base de datos", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_preguntas preguntas = new DB_preguntas(0, usuarioObjetivo, "0", "0", "0");
+            DataTable preguntaEspecifica = preguntas.GetIdPregunta(preguntas);
+            return preguntaEspecifica;
+        }
+
         public static string agregarUsuario(int miUsuarioID, int id, int tipoid, int documentoid, string username, string password, string nombres, string apellidos, string sexo, string nacimiento, int cedula, string direccion, string correo, string telefono, string buscar)
         {
             // Auditamos la Accion
@@ -98,6 +124,18 @@ namespace CapaNegocio
             return nuevoTipo.Create(nuevoTipo);
         }
 
+        public static string agregarPregunta(int miUsuarioID, int id, int iduser, string pregunta, string contrasena, string search)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miUsuarioID, _fechaActual, "Registro de una nueva pregunta de usuario", "El Usuario solicito el registro de una pregunta de seguridad de usuario", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_preguntas nuevaPregunta = new DB_preguntas(id, iduser, pregunta, contrasena, search);
+            return nuevaPregunta.Create(nuevaPregunta);
+        }
+
         public static string modificarUsuario(int miUsuarioID, int id, int tipoid, int documentoid, string username, string password, string nombres, string apellidos, string sexo, string nacimiento, int cedula, string direccion, string correo, string telefono, string buscar)
         {
             // Auditamos la Accion
@@ -108,6 +146,18 @@ namespace CapaNegocio
             // Ejecutamos la Accion
             DB_users actualUsuario = new DB_users(id, tipoid, documentoid, username, password, nombres, apellidos, sexo, nacimiento, cedula, direccion, correo, telefono, buscar);
             return actualUsuario.Edit(actualUsuario);
+        }
+
+        public static string modificarPregunta(int miUsuarioID, int id, int iduser, string pregunta, string contrasena, string search)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miUsuarioID, _fechaActual, "Modifico preguntas de seguridad de un Usuario", "El Usuario solicito modificar los datos de preguntas de seguridad de un usuario", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_preguntas preguntaUser = new DB_preguntas(id, iduser, pregunta, contrasena, search);
+            return preguntaUser.Edit(preguntaUser);
         }
 
         public static string eliminarUsuario(int miUsuarioID, int id)
@@ -121,5 +171,18 @@ namespace CapaNegocio
             DB_users eliminarUsuario = new DB_users(id, 0, 0, "0", "0", "0", "0", "0", "0", 0, "0", "0", "0", "0");
             return eliminarUsuario.Delete(eliminarUsuario);
         }
+
+        public static string eliminarPregunta(int miUsuarioID, int iduser)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miUsuarioID, _fechaActual, "Elimino preguntas de un Usuario", "El Usuario solicito eliminar los datos de pregunta de un usuario", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_preguntas eliminarPreguntas = new DB_preguntas(0, iduser, "0", "0", "0");
+            return eliminarPreguntas.Delete(eliminarPreguntas);
+        }
+
     }
 }
