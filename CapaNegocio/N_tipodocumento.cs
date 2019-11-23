d08971004164382b19c253c3a6a91a3117bb39f9
Robert Gonzalez <robert_gon97@hotmail.com>
2019-11-23 16:37:28 -0400

    Add documentos global

---
+++ CapaNegocio/N_tipodocumento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using System.Data;
+
+namespace CapaNegocio
+{
+    public class N_tipodocumento
+    {
+        public static DataTable obtenerTodos()
+        {
+            DataTable documentos = new DB_tipo_documento().GetAll();
+            return documentos;
+        }
+
+    }
+}
