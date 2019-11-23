cfbde0e38a122718366323235e44d6ca4602dbbb
Robert Gonzalez <robert_gon97@hotmail.com>
2019-11-23 16:34:50 -0400

    Add config

---
+++ CapaNegocio/N_configuracion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using System.Data;
+
+namespace CapaNegocio
+{
+    public class N_configuracion
+    {
+        public static DataTable obtenerConfiguracion()
+        {
+            DataTable Config = new DB_articulos().GetAll();
+            return Config;
+        }
+
+    }
+}
