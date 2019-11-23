7652ba5ff88aea37a2e4274d4f77c21393ceb5c7
Robert Gonzalez <robert_gon97@hotmail.com>
2019-11-23 15:28:20 -0400

    Add notas de credito al modelo ventas negocio

--- CapaNegocio/N_ventas.cs
+++ CapaNegocio/N_ventas.cs
@@ -99,8 +99,7 @@ namespace CapaNegocio
             DataTable DetalleVentaEnTabla = DetalleVenta.GetDetalleID(DetalleVenta);
             return DetalleVentaEnTabla;
         }
-
-
+        
         public static DataTable obtenerTodasLasVentasDetalles(int miUsuarioID)
         {
             // Auditamos la Accion
@@ -112,8 +111,7 @@ namespace CapaNegocio
             DataTable TodasLasVentasDetalles = new DB_ventas_detalles().GetAll();
             return TodasLasVentasDetalles;
         }
-
-
+        
         public static string agregarVentasDetalles(int miUsuarioID, int _detalle_venta_id, int _venta_id, int _stock_id, int _detalle_venta_cantidad, decimal _detalle_venta_precio_unidad, decimal _detalle_venta_precio_total, string buscar)
         {
             // Auditamos la Accion
@@ -125,5 +123,82 @@ namespace CapaNegocio
             DB_ventas_detalles nuevoDetalleVenta = new DB_ventas_detalles(_detalle_venta_id, _venta_id, _stock_id, _detalle_venta_cantidad, _detalle_venta_precio_unidad, _detalle_venta_precio_total, buscar);
             return nuevoDetalleVenta.Create(nuevoDetalleVenta);
         }
+
+        // Notas de credito
+
+        public static DataTable obtenerTodasLasNotasCredito(int miUsuarioID)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miUsuarioID, _fechaActual, "Obtuvo Todas Las Notas de Credito", "El Usuario solicito todas las notas de credito de la aplicacion", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DataTable NotasCreditos = new DB_venta_nota_credito().GetAll();
+            return NotasCreditos;
+        }
+
+        public static DataTable obtenerUnaNotaCredito(int miUsuarioID, int id)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miUsuarioID, _fechaActual, "Obtuvo informacion de una nota de credito", "El Usuario solicito los datos de una nota de credito", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_venta_nota_credito NotaCredito = new DB_venta_nota_credito(id, 0, "0", 0, 0, "0");
+            DataTable TablaNotas = NotaCredito.GetNOTAID(NotaCredito);
+            return TablaNotas;
+        }
+
+        public static DataTable buscarNotasCreditos(int miUsuarioID, string search)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miUsuarioID, _fechaActual, "Solicito buscar informacion de una nota de credito", "El Usuario solicito la busqueda de todos los datos de las notas de credito en la base de datos", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_venta_nota_credito NotaSearch = new DB_venta_nota_credito(0, 0, "0", 0, 0, search);
+            DataTable TablaNotas = NotaSearch.GetSearch(NotaSearch);
+            return TablaNotas;
+        }
+
+        public static string agregarNotaCredito(int miUsuarioID, int id, int ventaid, string fechanotacredito, decimal gastado, int valido, string buscar)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miUsuarioID, _fechaActual, "Registro una nueva nota de credito", "El Usuario solicito el registro de otra nota de credito", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_venta_nota_credito nuevaNotaCredito = new DB_venta_nota_credito(id, ventaid, fechanotacredito, gastado, valido, buscar);
+            return nuevaNotaCredito.Create(nuevaNotaCredito);
+        }
+
+        public static string modificarNotaCredito(int miUsuarioID, int id, int ventaid, string fechanotacredito, decimal gastado, int valido, string buscar)
+        {
+            // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miUsuarioID, _fechaActual, "Modifico informacion de una nota de credito", "El Usuario solicito modificar los datos de una nota de credito", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_venta_nota_credito NotaCreditoEdit = new DB_venta_nota_credito(id, ventaid, fechanotacredito, gastado, valido, buscar);
+            return NotaCreditoEdit.Edit(NotaCreditoEdit);
+        }
+
+        public static string eliminarNotaCredito(int miUsuarioID, int id)
+
+        { // Auditamos la Accion
+            string _fechaActual = DateTime.Now.ToLongDateString();
+            DB_auditoria nuevaAuditoria = new DB_auditoria(0, miUsuarioID, _fechaActual, "Elimino informacion de una nota de credito", "El Usuario solicito eliminar los datos de una nota de credito", "");
+            nuevaAuditoria.Create(nuevaAuditoria);
+
+            // Ejecutamos la Accion
+            DB_venta_nota_credito eliminarNota = new DB_venta_nota_credito(id, 0, "0", 0, 0, "0");
+            return eliminarNota.Delete(eliminarNota);
+        }
+
     }
 }
